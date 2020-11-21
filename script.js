fda9bf618d32a046f84b1f76acc838640a58d67c
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2020-11-21 12:13:34 -0500

    pseudocode

--- script.js
+++ script.js
@@ -6,5 +6,18 @@
 // uber api call using user location and brewery location
 // open uber app??
 
+var query = "https://api.uber.com/v1.2/requests/estimate";
 
+$.ajax({
+    url: query,
+    method: "POST",
+    data: {
+        "start_latitude": 37.7752278,
+        "start_longitude": -122.4197513,
+        "end_latitude": 37.7773228,
+        "end_longitude": -122.4272052
+    }
+}).then(function(response){
+    console.log(response);
+});
 
