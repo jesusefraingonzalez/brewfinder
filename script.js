e09552fa622d1ce8a66a5f0a6a5abc70ed6cc952
Laura Parker <lauramparker78@gmail.com>
2020-11-24 19:20:51 -0500

    Adding draft geolocation fucntion (lines 11-37)

--- script.js
+++ script.js
@@ -6,7 +6,59 @@
 
 
 
-//Ajax Calls / Functions
+//Functions
+
+// get user's GEOLOCATION, LAT & LONG
+
+function userLocation() {
+    if (navigator.geolocation) {
+
+       navigator.geolocation.getCurrentPosition(showPosition);
+        } 
+        else {
+          alert("Geolocation is not supported by this browser.");
+        }
+      }
+
+
+// This will get called after getCurrentPosition()
+function showPosition(position) {
+    // Grab coordinates from the given object
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    console.log(lat,lon); //checkpoint
+
+ // Call the distance function, passing on the coordinates 
+ distance(lat, lon);
+}
+
+function distance(lat, lon) {
+ 
+};
+
+
+
+
+//AJAX Calls
+
+// initialize open brew api call by user's state; calculate distance
+
+$.ajax({ //Current Day & City
+    
+    url: "https://api.openbrewerydb.org/breweries?by_state=" + userstate + "&per_page=50",
+    method: "GET"
+    })
+    .then(function(response) {
+        console.log(response); 
+        //for each 
+            var brewlon = response.list.longitude 
+            var brewlat = response.list.latitude 
+
+    });
+
+
+
+//click event to append brewery elements upon user entering their zipcode
 
 $("#zipbutton").on("click", function (event){  //need to match id to the input button
     event.preventDefault();
@@ -40,8 +92,8 @@ $.ajax({ //Current Day & City
 =======
 // JA.VUNmGAAAAAAAEgASAAAABwAIAAwAAAAAAAAAEgAAAAAAAAH4AAAAFAAAAAAADgAQAAQAAAAIAAwAAAAOAAAAzAAAABwAAAAEAAAAEAAAAJqGtdFpFrM2wYNPw_uyJoqnAAAAv90qsy_0wJLOkCdIsXZvLp4EVD9IfkIFI5U9Ze49UYdR2RCmilXEep7ljRaX_3hgClBvL8Scn8tXCkRsD2gW58bSc1wdvIxarb4_LatPvsW2I9J-94cUxCzkeGfgDQQ4BA9Ivu7m70UW9IQbejoCW7YFJn5R36PCXabwigA_4ijjBtQBSabTdNo_DR4lB6GQPdTkKb70lI_DWf21-dKU-60zpa0xtYgADAAAADmwkcQJnjXH8fFynSQAAABiMGQ4NTgwMy0zOGEwLTQyYjMtODA2ZS03YTRjZjhlMTk2ZWU
 
-// get user's geolocation, lat & long
-// open brew api call
+
+
 // get lat and long from open brew
 // uber api call using user location and brewery location
 // open uber app??
