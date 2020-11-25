08dd887f934359c458366e7db955bb8eb6f37143
Laura Parker <lauramparker78@gmail.com>
2020-11-24 20:25:10 -0500

    Edits on user location and brewery subset array

--- script.js
+++ script.js
@@ -21,20 +21,22 @@ function userLocation() {
       }
 
 
-// This will get called after getCurrentPosition()
-function showPosition(position) {
-    // Grab coordinates from the given object
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    console.log(lat,lon); //checkpoint
+    // This will get called after getCurrentPosition()
+    function showPosition(position) {
+        // Grab coordinates from the given object
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        console.log(lat,lon); //checkpoint
 
- // Call the distance function, passing on the coordinates 
- distance(lat, lon);
-}
+    // Call the distance function, passing on the coordinates 
+    distance(lat, lon);
+    };
 
+    
+// Calculates the distance between the user and the lat & lon subset of brewery listings
 function distance(lat, lon) {
- 
-};
+
+    };
 
 
 
@@ -43,14 +45,18 @@ function distance(lat, lon) {
 
 // initialize open brew api call by user's state; calculate distance
 
-$.ajax({ //Current Day & City
+$.ajax({ //Grabs subset of breweries based on user's initial location input
     
-    url: "https://api.openbrewerydb.org/breweries?by_state=" + userstate + "&per_page=50",
+    url: "https://api.openbrewerydb.org/breweries?by_postal_code=" + searchZip + "&per_page=50",
     method: "GET"
     })
     .then(function(response) {
         console.log(response); 
-        //for each 
+
+        var closestBrew = 
+
+
+        //for each brewery element in the closestBrew array
             var brewlon = response.list.longitude 
             var brewlat = response.list.latitude 
 
@@ -89,12 +95,9 @@ $.ajax({ //Current Day & City
     });
 
 }); //end click handler function
-=======
-// JA.VUNmGAAAAAAAEgASAAAABwAIAAwAAAAAAAAAEgAAAAAAAAH4AAAAFAAAAAAADgAQAAQAAAAIAAwAAAAOAAAAzAAAABwAAAAEAAAAEAAAAJqGtdFpFrM2wYNPw_uyJoqnAAAAv90qsy_0wJLOkCdIsXZvLp4EVD9IfkIFI5U9Ze49UYdR2RCmilXEep7ljRaX_3hgClBvL8Scn8tXCkRsD2gW58bSc1wdvIxarb4_LatPvsW2I9J-94cUxCzkeGfgDQQ4BA9Ivu7m70UW9IQbejoCW7YFJn5R36PCXabwigA_4ijjBtQBSabTdNo_DR4lB6GQPdTkKb70lI_DWf21-dKU-60zpa0xtYgADAAAADmwkcQJnjXH8fFynSQAAABiMGQ4NTgwMy0zOGEwLTQyYjMtODA2ZS03YTRjZjhlMTk2ZWU
 
 
 
-// get lat and long from open brew
 // uber api call using user location and brewery location
 // open uber app??
 
