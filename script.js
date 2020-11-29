0a890c84bc20f2c5ed34a6b2165ca261035bdd68
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2020-11-28 21:41:14 -0500

    populate brewery information html

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
 //VARIABLES
-
+//var searchzip= $("#zipButton").val().trim();
 
 //API Keys
 
@@ -8,57 +7,67 @@
 
 //Ajax Calls / Functions
 
-$("#zipbutton").on("click", function (event){  //need to match id to the input button
-    event.preventDefault();
-    
-    var searchzip = "97210" ;  //HARD CODED; need to determine user zip, then associated zip codes
+//$("#zipButton").on("click", function (event){  //need to match id to the input button
+//event.preventDefault();
 
-$.ajax({ //Current Day & City
-    
-
-    url: "https://api.openbrewerydb.org/breweries?by_postal=" + searchzip,
-    method: "GET"
+//var searchzip = "43215" ;  //HARD CODED; need to determine user zip, then associated zip codes
+function grabZip(searchZip) {
+    $.ajax({ //Current Day & City
+        url: "https://api.openbrewerydb.org/breweries?by_postal=" + searchZip,
+        method: "GET"
     })
-    .then(function(response) {
-        console.log(response);  // DRAFT this is just here for checks during development
+        .then(function (response) {
+            console.log(response);  // DRAFT this is just here for checks during development
 
-    //    for (var i = 0; i < response.length; i++) { //Need another loop/function to decide which breweries are in the recommended list based on distance
+            //    for (var i = 0; i < response.length; i++) { //Need another loop/function to decide which breweries are in the recommended list based on distance
+            for (var i = 0; i < response.length; i++) { //DRAFT.  
+                //create data for brewery cards
+                var newCard = $("<div>").attr("class", "uk-card-body");
+                var brewName = $("<p>").attr("id", "brewNameEL").html(response[i].name);
+                var brewType = $("<p>").attr("id", "brewType").html(response[i].brewery_type);
+                var brewAddress = $("<p>").attr("id", "addressEl").html(response[i].street + " " + response[i].city + " " + response[i].state + " " + response[i].postal_code + "</p>");
+                var brewPhone = $("<p>").attr("id", "phoneEl").html("(" + response[i].phone.substring(0, 3) + ") " + response[i].phone.substring(3, 6) + "-" + response[i].phone.substring(6, 10));
+                var brewUrl = $("<a>").attr("href", response[i].website_url).attr("id", "webEl").html(response[i].website_url);
 
-                for (var i = 0; i < 11; i++) { //DRAFT.  
+                newCard.append(brewName);
+                newCard.append(brewType);
+                newCard.append(brewAddress);
+                newCard.append(brewPhone);
+                newCard.append(brewUrl);
 
-                    //appending data to brewery cards
-                    $("#brewNameEl").append("<h6>" + response.list[i].name + "<h6> " + "</br>");
-                            
+                $("body").append(newCard);
+            }
+        });
+}
 
-                };
+//end click handler function
 
-     //   };  //end outer loop      
+$("#submitZip").on("click", function (event) {
+    event.preventDefault();
+    var zipC = $("#zipButton").val();
 
+    grabZip(zipC);
+});
+
+$("#webEl").click(function (event) {
+    event.preventDefault();
+    console.log($(this));
+    var barLink = $(this).val();
+    // link api query
+    var linkApiKey = "add99689022bb0b11c5fd0a126838bc8";
+    var linkQuery = "http://api.linkpreview.net/?key=" + linkApiKey + "&q=" + barLink;
+
+    // TODO get the requested link from the dom
+
+    // ajax query to get the link preview from the api
+    $.ajax({
+        url: linkQuery,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
     });
-
-}); //end click handler function
-=======
-// JA.VUNmGAAAAAAAEgASAAAABwAIAAwAAAAAAAAAEgAAAAAAAAH4AAAAFAAAAAAADgAQAAQAAAAIAAwAAAAOAAAAzAAAABwAAAAEAAAAEAAAAJqGtdFpFrM2wYNPw_uyJoqnAAAAv90qsy_0wJLOkCdIsXZvLp4EVD9IfkIFI5U9Ze49UYdR2RCmilXEep7ljRaX_3hgClBvL8Scn8tXCkRsD2gW58bSc1wdvIxarb4_LatPvsW2I9J-94cUxCzkeGfgDQQ4BA9Ivu7m70UW9IQbejoCW7YFJn5R36PCXabwigA_4ijjBtQBSabTdNo_DR4lB6GQPdTkKb70lI_DWf21-dKU-60zpa0xtYgADAAAADmwkcQJnjXH8fFynSQAAABiMGQ4NTgwMy0zOGEwLTQyYjMtODA2ZS03YTRjZjhlMTk2ZWU
-
-// get user's geolocation, lat & long
-// open brew api call
-// get lat and long from open brew
-// uber api call using user location and brewery location
-// open uber app??
-
-var query = "https://api.uber.com/v1.2/requests/estimate";
-
-$.ajax({
-    url: query,
-    method: "POST",
-    data: {
-        "start_latitude": 37.7752278,
-        "start_longitude": -122.4197513,
-        "end_latitude": 37.7773228,
-        "end_longitude": -122.4272052
-    }
-}).then(function(response){
-    console.log(response);
 });
 
 
+
+grabZip("43215");
